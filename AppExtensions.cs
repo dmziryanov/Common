e057fb0861834e9b75098400c0a8f15996be76f0
Dmitriy.Ziryanov <Dmitriy.Ziryanov@indusoft.ru>
2019-10-24 10:07:58 +0300

    First commit

---
+++ AppExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Indusoft.CalendarPlanning.Common
+{
+    public static class AppExtensions
+    {
+        private static void PerformMigrations<T>(this IApplicationBuilder app) where T : DbContext
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var ctx = serviceScope.ServiceProvider.GetRequiredService<T>();
+                ctx.Database.Migrate();
+            }
+        }
+    }
+}
